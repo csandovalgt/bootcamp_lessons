82aa0b4172dee9ad1531aa6cc0f631871198a2f7
Carlos Sandoval <carlossmetal@gmail.com>
2023-07-28 19:46:15 -0600

    m2s7

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:typed_data';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -34,125 +37,79 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       backgroundColor: Colors.grey,
       appBar: AppBar(
-        title: const Text("Bootcamp S7 M2"),
+        title: const Text("Bootcamp S8 M2"),
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            const SizedBox(
-              width: 200,
-              height: 75,
-              child: Card(
-                elevation: 5,
-                child: Center(
-                  child: Text("Soy una card"),
-                ),
-              ),
-            ),
-            const AspectRatio(
-              aspectRatio: 5 / 1,
-              child: Card(
-                elevation: 5,
-                child: Center(
-                  child: Text("Soy una card"),
-                ),
-              ),
-            ),
-            ConstrainedBox(
-              constraints: const BoxConstraints(
-                minHeight: 100,
-                maxHeight: 200,
-                minWidth: 100,
-                maxWidth: 200,
-              ),
-              child: Container(
-                width: 50,
-                height: 150,
-                color: Colors.indigo,
-              ),
-            ),
-            AspectRatio(
-              aspectRatio: 6 / 1,
-              child: Container(
-                color: Colors.yellow,
-                child: Column(
-                  children: const [
-                    Text("Hola a todos"),
-                    Text("Hola solo a mi"),
-                  ],
-                ),
-              ),
-            ),
             SizedBox(
-              width: double.infinity,
-              height: 90,
-              child: Align(
-                  alignment: Alignment.topRight,
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 8),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text("Clickeame 1"),
+              height: 20,
+            ),
+            Center(
+              child: CustomPaint(
+                painter: MyCustomPainter(),
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  // color: Colors.white,
+                ),
+              ),
+            ),
+            DecoratedBox(
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(
+                  30
+                )
+              ),
+              child: Image.network(
+                'https://docs.flutter.dev/assets/images/dash/Dash.png',
+                width: 200,
+                height: 200,
+              ),
+            )
+            /*
+            Stack(
+              children: [
+                Image.network(
+                  'https://docs.flutter.dev/assets/images/dash/Dash.png',
+                ),
+                Positioned.fill(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                    child: Container(
+                      height: 200,
+                      color: Colors.black.withOpacity(0),
                     ),
-                  )),
+                  ),
+                ),
+              ],
             ),
-            SizedBox(
-              width: double.infinity,
-              height: 90,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text("Clickeame 2"),
-              ),
-            ),
-            SizedBox(
-              width: 50,
-              height: 100,
-              child: Container(
-                color: Colors.purple,
-              ),
-            ),
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.deepOrangeAccent,
-            ),
-            AspectRatio(
-              aspectRatio: 5 / 1,
+            ClipOval(
+              clipper: MyCustomClipper(),
               child: Image.network(
                 'https://docs.flutter.dev/assets/images/dash/Dash.png',
               ),
             ),
-            Align(
-              child: Container(
-                color: Colors.blue,
-                height: 500,
-                width: 300,
-                child: FittedBox(
-                  fit: BoxFit.fitWidth,
-                  clipBehavior: Clip.hardEdge,
-                  child: Image.network(
-                    'https://docs.flutter.dev/assets/images/dash/Dash.png',
-                    width: 50,
-                    height: 50,
-                  ),
+            ClipPath(
+              clipper: MyCustomPathClipper(),
+              child: Image.network(
+                'https://docs.flutter.dev/assets/images/dash/Dash.png',
+                width: 200,
+                height: 200,
+              ),
+            ),
+            ClipRect(
+              clipper: MyCustomClipper(),
+              child: Align(
+                alignment: Alignment.topCenter,
+                heightFactor: 0.1,
+                child: Image.network(
+                  'https://docs.flutter.dev/assets/images/dash/Dash.png',
                 ),
               ),
             ),
-            Align(
-              child: Container(
-                color: Colors.green,
-                height: 500,
-                width: 300,
-                child: FittedBox(
-                  fit: BoxFit.fill,
-                  child: Image.network(
-                    'https://docs.flutter.dev/assets/images/dash/Dash.png',
-                    width: 50,
-                    height: 50,
-                  ),
-                ),
-              ),
-            )
+*/
           ],
         ),
       ),
@@ -160,5 +117,55 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-// 200 ancho: 150 alto
-// carta: 5/1
+class MyCustomClipper extends CustomClipper<Rect> {
+  @override
+  Rect getClip(Size size) {
+    return Rect.fromCenter(center: Offset(200, 200), width: 100, height: 100);
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Rect> oldClipper) {
+    return true;
+  }
+}
+
+class MyCustomPathClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    print(size);
+    Path path = Path();
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0.0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
+
+class MyCustomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    print(size);
+    final paint = Paint()
+      ..color = Colors.blue
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 8.0;
+
+    final path = Path()
+      ..lineTo(0, 100)
+      ..lineTo(100, 100)
+      ..lineTo(100, 0)
+      ..lineTo(0, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
+}
